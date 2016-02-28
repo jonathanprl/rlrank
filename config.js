99a65e6e5a6c9310b8d784e6f259fd4013eb5c98
Jonathan Lambert <jonathan@swiftping.co.uk>
2016-02-28 18:49:45 +0000

    Updated port to 80

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    test: 'Hello',
-    port: 8080,
+    port: 80,
     1: 'f:@i!EgC)S4DPO)',
-    services: {
-      qpx: {
-        key: 'AIzaSyDg1CrKmm9o5Jf2GeSyWt8hZsGtDye-nHo',
-        endpoint: 'https://www.googleapis.com/qpxExpress/v1/trips'
-      }
-    },
     mongodb: {
       connection: 'mongodb://localhost:27017/rlrank'
     }
