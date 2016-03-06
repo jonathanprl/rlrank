3c4fa478bf4905235bcb3cafd6c00123242cb6d6
Jonathan Lambert <jonathan@swiftping.co.uk>
2016-03-06 18:06:02 +0000

    [DEV] Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+          2,
+          { allow: ["log", "error"] }
+        ]
+    }
+};
