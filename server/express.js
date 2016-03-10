5d7870543de8253b054d2c6fc3108648c99d2c34
Jonathan Lambert <jonathan@swiftping.co.uk>
2016-03-10 22:34:26 +0000

    [BUGFIX] Host sparkline locally https

--- server/express.js
+++ server/express.js
@@ -6,11 +6,12 @@ var bodyParser = require('body-parser');
 var env = process.env.ENV || 'dev';
 var config = require('../config');
 var path = require('path');
-var enforce = require('express-sslify');
+
+console.log(config.env);
 
 app.set('views', path.normalize(__dirname + '/../public/app'));
 app.set('view engine', 'jade');
-if (config.env == 'prod') app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (config.env == 'prod') app.use(require('express-sslify').HTTPS({ trustProtoHeader: true }));
 app.use(require('prerender-node').set('prerenderToken', 'It894S0HIa5KY4kogyI2'));
 app.use(bodyParser.json());
 app.use(express.static(path.normalize(__dirname + '/../public')));
