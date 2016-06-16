e04292b4d22ac99d7f8fc7fdc10ae840b5a2da44
Jonathan Lambert <jonathan@swiftping.co.uk>
2016-06-16 01:12:41 +0100

    [SEO] Blog title and description

--- public/app/blog/blog.ctrl.js
+++ public/app/blog/blog.ctrl.js
@@ -1,13 +1,16 @@
 (function() {
   angular
     .module('app')
-    .controller('BlogController', ['BlogSvc', 'Analytics', 'AmazonSvc', BlogController]);
+    .controller('BlogController', ['BlogSvc', 'Analytics', 'AmazonSvc', 'TitleSvc', '$routeParams', BlogController]);
 
-  function BlogController(BlogSvc, Analytics, AmazonSvc)
+  function BlogController(BlogSvc, Analytics, AmazonSvc, TitleSvc, $routeParams)
   {
     'use strict';
 
     var vm = this;
+
+    TitleSvc.setTitle('Blog');
+    TitleSvc.setDescription('Rocket League articles featuring tips on how to build your Rocket League stats and climb the rankings, hardware and more.');
 
     BlogSvc.getPosts()
       .then(function(response) {
@@ -16,6 +19,12 @@
         AmazonSvc.shortcodes(vm.posts[0].content, function(err, content) {
           vm.posts[0].content = content;
         });
+
+        if ($routeParams.seo_title)
+        {
+          TitleSvc.setTitle(vm.posts[0].title);
+          TitleSvc.setDescription(vm.posts[0].content.substring(0,300));
+        }
       })
       .catch(function(err) {
         console.log(err);
