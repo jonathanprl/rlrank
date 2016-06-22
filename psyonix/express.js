26e278b7a0244db17fb30768087626b8ccf03c18
Jonathan Lambert <jonathan@swiftping.co.uk>
2016-06-22 21:10:07 +0100

    [PSYNODE] CORS

--- psyonix/express.js
+++ psyonix/express.js
@@ -6,6 +6,11 @@ var bodyParser = require('body-parser');
 var config = require('./config');
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.enable('trust proxy');
 app.listen(config.port);
 
